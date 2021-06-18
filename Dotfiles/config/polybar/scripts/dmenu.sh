414260f60cefb1ea36121527b4d33510b99bc870
TheRealHex <devilxnp@gmail.com>
2021-06-18 21:03:02 +0545

    Config

---
+++ Dotfiles/config/polybar/scripts/dmenu.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# A dmenu wrapper script for system functions.
+
+# For non-systemd init systems.
+case "$(readlink -f /sbin/init)" in
+	*runit*) hib="sudo -A zzz" ;;
+	*openrc*) reb="loginctl reboot"; shut="loginctl poweroff" ;;
+esac
+
+cmds="\
+  lock		slock
+  reboot	${reb:-sudo -A reboot}
+  Poweroff	${shut:- sudo shutdown now}
+  display off 	 xset dpms force off"
+
+choice="$(echo "$cmds" | cut -d'	' -f 1 | dmenu -p BYE -l 6 )" || exit 1
+
+`echo "$cmds" | grep "^$choice	" | cut -d '	' -f2-`
