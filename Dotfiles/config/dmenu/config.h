8ad8a57cb100ed65da8623a0ba0c63f4b42b8807
TheRealHex <devilxnp@gmail.com>
2021-11-11 15:37:55 +0545

    Font size 9

--- Dotfiles/config/dmenu/config.h
+++ Dotfiles/config/dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CaskaydiaCove Nerd Font:style=Bold:size=9.5"
+	"CaskaydiaCove Nerd Font:style=Bold:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
