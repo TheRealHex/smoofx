5dfac29cafa5c0a92cf3111fad937c3623ad4548
TheRealHex <devilxnp@gmail.com>
2024-04-04 09:36:19 +0545

    update

--- Dotfiles/config/polybar/launch.sh
+++ Dotfiles/config/polybar/launch.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
+# Restart polybar left and right 
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+polybar left 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar2.log & disown
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
-echo "Bars launched..."
